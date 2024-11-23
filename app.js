9f7da464faf8facd7b8064b708b8e1fb3e99c470
MuhSaktiR <seken876@gmail.com>
2024-11-23 16:47:00 +0700

    Memperbaiki typo nama database

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',     
   password: process.env.DB_PASSWORD || '', 
-  database: process.env.DB_NAME || 'nama_database',
+  database: process.env.DB_NAME || 'pertanian',
 });
 
 connection.connect((err) => {
